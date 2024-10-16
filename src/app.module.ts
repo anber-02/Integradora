0f1f5091cff3489906f9b30dcbb16ca52153522e
anber-02 <newprogrammer02@gmail.com>
2024-10-15 18:59:33 -0600

    Creacion del modulo Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    // JWT
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService
+  ]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/modules/user/entities/user.entity.ts
@@ -0,0 +1,42 @@
+import { Column, Entity, JoinTable, ManyToMany, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
+import { Role } from 'src/modules/roles/entities/role.entity'
+
+@Entity({name:"users"})
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  name: string
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+  @Column()
+  phone_number: string
+  
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  created_at: Date;
+
+  @ManyToMany(() => Role, roles => roles.users)
+  @JoinTable({
+      name: 'rol_user',
+      joinColumn: {
+        name: 'user_id',
+        referencedColumnName: 'id'
+      },
+      inverseJoinColumn: {
+        name: 'rol_id',
+        referencedColumnName: "id"
+      }
+    })
+  roles: Role[]
+
+}
+
