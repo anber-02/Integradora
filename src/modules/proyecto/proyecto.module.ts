209ac5f88af8f8782c938fc181da266b584167c2
anber-02 <newprogrammer02@gmail.com>
2024-11-13 13:26:47 -0600

    fix: Corrigiendo un error en el modulo de proyectos con la authenticacion

--- src/modules/proyecto/proyecto.module.ts
+++ src/modules/proyecto/proyecto.module.ts
@@ -3,9 +3,10 @@ import { ProyectoService } from './proyecto.service';
 import { ProyectoController } from './proyecto.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Proyecto } from './entities/proyecto.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Proyecto])],
+  imports: [TypeOrmModule.forFeature([Proyecto]), AuthModule],
   controllers: [ProyectoController],
   providers: [ProyectoService],
 })
